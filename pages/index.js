f44a07a07a77c65ec362f582e60f6bca7fb8c756
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2022-04-03 18:14:40 -0400

    created _app.js and wrote header in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-    return <div>Hello</div>
+    return (
+    <Container>
+        <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            Hello, I am a full-stack developer based in Montreal.
+        </Box>
+
+        <Box display={{md:'flex'}}>
+            <Box flexGrow={1}>
+                <Heading as="h2" variant="page-title">
+                    Patrick-Lam Nguyen
+                </Heading>
+                <p>Placeholder Text for now</p>
+            </Box>
+        </Box>
+    </Container>
+    )
 }
 
 export default Page
