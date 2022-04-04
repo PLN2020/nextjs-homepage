cdbf6e000e611bd83e14a07ddaac38bd627b1f55
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2022-04-04 04:33:33 -0400

    created the theme toggle button component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "227d07e69c93e8c8"; }
+/******/ 		__webpack_require__.h = function() { return "0bcac7de0d10e45f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
