0311cdab3ae43205717f0a26b9d6a7bf40f397a1
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2022-04-04 05:06:35 -0400

    Font fixed for theme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0a083d44899a734"; }
+/******/ 		__webpack_require__.h = function() { return "e6e9f00f6c91fcdf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
